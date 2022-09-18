d899f2845575d7ab0579ada1c2b7ba3e965fdf7a
shubham maithani <shubhammaithani1@gmail.com>
2022-09-18 02:55:21 -0700

    Find leap year

--- BasicCore_program.java
+++ BasicCore_program.java
@@ -6,31 +6,15 @@ public class BasicCore_program {
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome To Basic Core program");
-		
-		int h_cnt = 0;
-		int t_cnt = 0;
-		
-		Scanner Scan = new Scanner(System.in);
-		System.out.println("Enter the no of times to flip a coin:");
-		
-		int n = Scan.nextInt();
-		
-		for (int i = 1; i < n; i++) {
-			double flip = Math.floor(Math.random() * 10) % 2;
-			if (flip < 0.5) {
-				h_cnt++;
-			} else {
-				t_cnt++;
-			}
+		int year;
 
-		}
-		double headpercentage = (h_cnt * 100) / n;
-		double tailpercentage = (t_cnt * 100) / n;
-		
-		System.out.println("The percentage of head flips:" + headpercentage);
-		System.out.println("The percentage of head flips:" + tailpercentage);
+		System.out.println("Enter an Year : ");
+		Scanner sc = new Scanner(System.in);
+		year = sc.nextInt();
 
+		if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+			System.out.println("This year is a leap year");
+		else
+			System.out.println("This year is not a leap year");
 	}
-
 }

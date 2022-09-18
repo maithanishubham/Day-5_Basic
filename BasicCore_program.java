9841f122b85846f485fa5f98cc30eecb7ef51af2
shubham maithani <shubhammaithani1@gmail.com>
2022-09-18 03:29:57 -0700

    Largest among 3 numbers

--- BasicCore_program.java
+++ BasicCore_program.java
@@ -4,12 +4,18 @@ public class BasicCore_program {
 
 	public static void main(String[] args) {
 
-		char ch = 'h';
+		int n1 = 4;
+		int n2 = 3;
+		int n3 = 5;
 
-		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
-			System.out.println(ch + " is vowel");
+		if (n1 >= n2 && n1 >= n3)
+			System.out.println(n1 + " is the largest number.");
+
+		else if (n2 >= n1 && n2 >= n3)
+			System.out.println(n2 + " is the largest number.");
+
 		else
-			System.out.println(ch + " is consonant");
+			System.out.println(n3 + " is the largest number.");
 
 	}
 
